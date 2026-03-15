0d8644c59d311a9d6070a18b33fce322adc3c68e
vunnam harikrishna <vunnamharikrishna1619@gmail.com>
2026-03-15 06:40:02 +0000

    Link is not defined
    src/app/page.tsx (167:14) @ HomePage
    
      165 |

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from 'react';
+import Link from 'next/link';
 import UserNavbar from '@/components/UserNavbar';
 import BannerCarousel from '@/components/BannerCarousel';
 import PropertyCard from '@/components/PropertyCard';
